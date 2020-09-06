39a9a544175244b6b601d1b0b09ae13495cf4539
Winnie Ku <winn.ku@gmail.com>
2020-09-06 08:49:50 -0400

    added Footer component

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <p>Created by Winnie Ku</p>
+            <p>Built using the OMDb API</p>
+        </Fragment>
+    );
+}
+
+export default Footer;
