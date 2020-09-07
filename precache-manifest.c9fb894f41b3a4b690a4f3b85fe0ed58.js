69765e446fff4f8cfe897e7bdbd07f751a17c6ad
Winnie Ku <winn.ku@gmail.com>
2020-09-07 15:30:25 -0400

    Updates

---
+++ precache-manifest.c9fb894f41b3a4b690a4f3b85fe0ed58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2450c8fab4f38fa8e13584a03ffac02",
+    "url": "/shopify-tech-challenge/index.html"
+  },
+  {
+    "revision": "4b951529d3ddc4dd3144",
+    "url": "/shopify-tech-challenge/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "bdba8c72677ed0880e74",
+    "url": "/shopify-tech-challenge/static/css/main.79845b23.chunk.css"
+  },
+  {
+    "revision": "4b951529d3ddc4dd3144",
+    "url": "/shopify-tech-challenge/static/js/2.9370650a.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/shopify-tech-challenge/static/js/2.9370650a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdba8c72677ed0880e74",
+    "url": "/shopify-tech-challenge/static/js/main.eb0c18c9.chunk.js"
+  },
+  {
+    "revision": "ce6a1818cfaa8a008abd",
+    "url": "/shopify-tech-challenge/static/js/runtime-main.23bd6645.js"
+  }
+]);
